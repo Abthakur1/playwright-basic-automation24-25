b34883a611191d58ca3c37041aff1f27dc5d3839
Abhishek <absk.cts@gmail.com>
2025-03-15 13:38:55 +0530

    ui and api integration login with api only

---
+++ tests/webApiTest.spec.ts
@@ -0,0 +1,47 @@
+import { test, expect, request } from "@playwright/test";
+const loginPayload = {
+  userEmail: "anshika@gmail.com",
+  userPassword: "Iamking@000",
+};
+let token;
+
+test.beforeAll(async () => {
+  const apiContext = await request.newContext();
+  const loginResponse = await apiContext.post(
+    "https://rahulshettyacademy.com/api/ecom/auth/login",
+    {
+      data: loginPayload,
+    }
+  );
+
+  console.log(loginResponse.status());
+  expect(loginResponse.status()).toBe(200);
+  const loginResponseJson = await loginResponse.json();
+  token = loginResponseJson.token;
+});
+
+
+test("Client App Test", async ({page}) =>{
+  await page.addInitScript(value=>{      // ==> to store the cookies in local storage
+    window.localStorage.setItem('token', value );
+  }, token)
+  
+  await page.goto('https://rahulshettyacademy.com/client/');
+  const expectedList = [
+    'qwerty',
+    'qwerty',
+    'qwerty',
+    'qwerty',
+    'ZARA COAT 3',
+    'ADIDAS ORIGINAL',
+    'IPHONE 13 PRO',
+    'qwerty'
+  ]
+await page.locator('div.card-body b').first().waitFor();
+const productNames = await page.locator('div.card-body b').allTextContents();
+productNames.forEach((item)=>{
+    const result = expectedList.includes(item);
+    expect(result).toBeTruthy;
+});
+
+});
